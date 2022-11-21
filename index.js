08ee5dbb6f42a8960e6931194b188916b521c613
Vlad <armagiddon19@gmail.com>
2022-11-21 14:18:56 +0300

    fix some problem with router

--- index.js
+++ index.js
@@ -8,41 +8,47 @@ let currentPath = null;
 let beforeRouterFunc = null;
 let mode = null;
 
-function changeState(state, link) {
+function changeState(state, link, component) {
   if (mode === "hash") {
     window.location.hash = "#" + link
   } else {
     history[state](null, null, link);
   }
   currentPath = link;
+  routerL.value = component;
 }
 
 const recursiveRouting = (name) => (path) => {
   const afterRoute = inputRouters.find(e => e.path === path);
   const beforeRoute = inputRouters.find(e => e.path === currentPath);
-
-  if (currentPath !== undefined) {
+  if (currentPath || afterRoute && !currentPath) {
     if (currentPath !== path) {
-      if (afterRoute.redirect !== undefined) {
+      if (afterRoute.redirect) {
         return recursiveRouting("pushState")(afterRoute.redirect);
       }
-      changeState(name, path);
-      routerL.value = afterRoute.component;
+      changeState(name, path, afterRoute.component);
       if (beforeRouterFunc !== null) {
         beforeRouterFunc(beforeRoute, afterRoute);
       }
     } else {
-      console.error("Вы пытаетесь пройти по текущему router. операция прервана")
+      //console.error("Вы пытаетесь пройти по текущему router. операция прервана")
     }
   } else {
-    er(`${current} - не найден`);
+    er(`${currentPath} - не найден`);
   }
 }
 
 function windowEvent () {
-  window.onpopstate = function() {
-    const current = mode === "history" ? document.location.pathname : document.location.hash.replace("#", "");
-    recursiveRouting("pushState")(current);
+  if (mode === "history") {
+    window.onpopstate = function() {
+      const current = document.location.pathname;
+      recursiveRouting("pushState")(current);
+    }
+  } else {
+    window.addEventListener("hashchange", () => {
+      const current = document.location.hash.replace("#", "");
+      recursiveRouting("pushState")(current);
+    })
   }
 }
 
@@ -64,13 +70,12 @@ export const createRouter = function(inputArray, m = "history") {
 
   let findComponent = inputArray.find(routerObject => routerObject.path === Path);
   let curRouter = null;
-
   if (findComponent !== undefined) {
     if(findComponent.component !== undefined) {
       routerL.value = findComponent.component;
       curRouter = findComponent.path;
     } else if (findComponent.redirect !== undefined) {
-      recursiveRouting("replaceState")(findComponent.redirect)
+      recursiveRouting("replaceState")(findComponent.redirect);
     }
   }
   currentPath = curRouter;
